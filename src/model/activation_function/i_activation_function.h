85edb5c4e68c17785d1dd8820a7154fc82d34b8e
yourlx <114220266+yourlx@users.noreply.github.com>
2023-09-12 15:58:30 +0700

    feat: add overload for activation function with vector

--- src/model/activation_function/i_activation_function.h
+++ src/model/activation_function/i_activation_function.h
@@ -8,7 +8,9 @@ class IActivationFunction {
   virtual ~IActivationFunction() = default;
 
   virtual double Activate(double value) = 0;
+  virtual void Activate(std::vector<double> &values) = 0;
   virtual double Derivative(double value) = 0;
+  virtual void Derivative(std::vector<double> &values) = 0;
 };
 
 #endif  // MULTILAYER_PERCEPTRON_CPP_QT_I_ACTIVATION_FUNCTION_H
--- src/model/activation_function/sigmoid.h
+++ src/model/activation_function/sigmoid.h
@@ -5,7 +5,9 @@
 
 class Sigmoid : public IActivationFunction {
   double Activate(double value) override;
+  void Activate(std::vector<double> &values) override;
   double Derivative(double value) override;
+  void Derivative(std::vector<double> &values) override;
 };
 
 #endif  // MULTILAYER_PERCEPTRON_CPP_QT_SIGMOID_H
