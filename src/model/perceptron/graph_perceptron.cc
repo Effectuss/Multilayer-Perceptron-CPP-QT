c735a34840a69d514a7a640d972846a42a045795
yourlx <114220266+yourlx@users.noreply.github.com>
2023-09-12 18:50:56 +0700

    feat: add layer/neuron as inner private classes
    Also provide some method for layer

--- src/model/perceptron/graph_perceptron.cc
+++ src/model/perceptron/graph_perceptron.cc
@@ -1,1 +1,67 @@
 #include "graph_perceptron.h"
+
+#include <algorithm>
+#include <random>
+
+GraphPerceptron::GraphPerceptron(IActivationFunction *activationFunction,
+                                 const Dataset &dataset, const Mapping &mapping,
+                                 int hidden_layers_count,
+                                 int size_hidden_layers) {
+  //  layer_count_ = 2 + hidden_layers_count;
+}
+
+int GraphPerceptron::Predict(Picture picture) {
+  // todo
+  return 0;
+}
+
+void GraphPerceptron::Train(int epochs) {
+  // todo
+}
+
+void GraphPerceptron::LoadWeights(const std::istream &) {
+  // todo
+}
+
+void GraphPerceptron::ExportWeights(const std::ostream &) {
+  // todo
+}
+
+void GraphPerceptron::FeedForward() {
+  // todo
+}
+
+void GraphPerceptron::PropagateBackwards() {
+  // todo
+}
+
+GraphPerceptron::Layer::Layer(int neuron_amount) {
+  neurons_.resize(neuron_amount);
+}
+
+void GraphPerceptron::Layer::ConnectLayer(Layer &previous) {
+  for (auto &neuron : neurons_) {
+    neuron.previous_neurons_.resize(previous.neurons_.size());
+    std::transform(previous.neurons_.begin(), previous.neurons_.end(),
+                   neuron.previous_neurons_.begin(),
+                   [](Neuron &n) { return &n; });
+  }
+}
+
+// todo: "true" randomness
+void GraphPerceptron::Layer::GenerateWeights() {
+  double lower = -0.5;
+  double upper = 0.5;
+
+  std::uniform_real_distribution<double> dist(lower, upper);
+  static std::random_device rd_;
+  static std::mt19937 generator_;
+
+  for (auto &neuron : neurons_) {
+    neuron.previous_neurons_weights_.resize(neuron.previous_neurons_.size());
+    std::transform(neuron.previous_neurons_weights_.begin(),
+                   neuron.previous_neurons_weights_.end(),
+                   neuron.previous_neurons_weights_.begin(),
+                   [](double) { return 5; });
+  }
+}
--- src/model/perceptron/graph_perceptron.h
+++ src/model/perceptron/graph_perceptron.h
@@ -1,10 +1,56 @@
 #ifndef GRAPH_PERCEPTRON_GRAPH_PERCEPTRON_H
 #define GRAPH_PERCEPTRON_GRAPH_PERCEPTRON_H
 
+#include "dataset.h"
+#include "i_activation_function.h"
 #include "i_perceptron.h"
+#include "mapping.h"
 
 class GraphPerceptron final : IPerceptron {
-  // todo
+ public:
+  GraphPerceptron() = delete;
+  GraphPerceptron(IActivationFunction *activationFunction,
+                  const Dataset &dataset, const Mapping &mapping,
+                  int hidden_layers_count, int size_hidden_layers);
+  ~GraphPerceptron() override = default;
+
+  int Predict(Picture picture) override;
+  void Train(int epochs) override;
+  void LoadWeights(const std::istream &) override;
+  void ExportWeights(const std::ostream &) override;
+
+ private:
+  class Layer final {
+   public:
+    Layer() = delete;
+    explicit Layer(int neuron_amount);
+
+    void ConnectLayer(Layer &previous);
+    void GenerateWeights();
+
+   private:
+    // friend class GraphPerceptron;
+
+    class Neuron final {
+     public:
+     private:
+      friend class Layer;
+
+      double value_ = 0;
+      double error_ = 0;
+      double weights_delta_ = 0;
+      std::vector<double> previous_neurons_weights_;
+      std::vector<Neuron *> previous_neurons_;
+    };
+
+    std::vector<Neuron> neurons_;
+  };
+
+  void FeedForward();
+  void PropagateBackwards();
+
+  // do I really need this field?
+  // int layer_count_;
 };
 
 #endif  // GRAPH_PERCEPTRON_GRAPH_PERCEPTRON_H
