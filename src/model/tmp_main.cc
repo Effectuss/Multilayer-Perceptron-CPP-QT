29002f5757d09e9bb0373531e907ee78a427b93f
progbagger <thrixstudio@yandex.ru>
2023-11-11 18:57:28 +0700

    fix: tmp_main.cc now compiles correctly

--- src/model/tmp_main.cc
+++ src/model/tmp_main.cc
@@ -5,29 +5,30 @@
 #include "sigmoid.h"
 
 int main() {
-    std::unique_ptr<IActivationFunction> sigmoid = std::make_unique<Sigmoid>();
+  std::unique_ptr<IActivationFunction> sigmoid = std::make_unique<Sigmoid>();
 
-    Dataset dataset = Parser::ParseDataset(
-            "/Users/englishk/goinfre/emnist-letters/emnist-letters-train.csv");
-    Dataset test_dataset = Parser::ParseDataset(
-            "/Users/englishk/goinfre/emnist-letters/emnist-letters-test.csv");
+  Mapping mapping = Parser::ParseMapping(
+      "/Users/englishk/goinfre/emnist-letters/emnist-letters-mapping.txt");
 
-    Mapping mapping = Parser::ParseMapping(
-            "/Users/englishk/goinfre/emnist-letters/emnist-letters-mapping.txt"
-    );
+  Dataset dataset = Parser::ParseDataset(
+      "/Users/englishk/goinfre/emnist-letters/emnist-letters-train.csv",
+      mapping.GetMinIndex());
 
-    MatrixPerceptron perceptron(3, 144, mapping, sigmoid);
+  Dataset test_dataset = Parser::ParseDataset(
+      "/Users/englishk/goinfre/emnist-letters/emnist-letters-test.csv",
+      mapping.GetMinIndex());
 
-    perceptron.LoadWeights("/Users/englishk/goinfre/weight.txt");
+  MatrixPerceptron perceptron(3, 144, mapping, sigmoid);
 
-//    for (int i = 0; i < 3; ++i) {
-//        perceptron.Train(1, dataset);
-        double r_p = perceptron.TestMatrixPerceptron(test_dataset);
-        std::cout << "AFTER TEST: " << r_p << "%" << std::endl;
-//    }
+  perceptron.LoadWeights("/Users/englishk/goinfre/weight.txt");
 
+  //    for (int i = 0; i < 3; ++i) {
+  //        perceptron.Train(1, dataset);
+  double r_p = perceptron.TestMatrixPerceptron(test_dataset);
+  std::cout << "AFTER TEST: " << r_p << "%" << std::endl;
+  //    }
 
-//    perceptron.ExportWeights("/Users/englishk/goinfre/weight.txt");
+  //    perceptron.ExportWeights("/Users/englishk/goinfre/weight.txt");
 
-    return 1;
+  return 1;
 }
